fa723963cd1207c0537cfd66b38f0a2791edd2b6
ThiagoFPenteado <thiagofp13@gmail.com>
2019-08-08 13:30:43 -0300

    Arquivos referente a C# Parte 9 - Entrada e saída (I/O) com streams do curso .Net da ALURA

---
+++ C# - .NET/C# Parte 9 - Entrada e saída (IO) com streams/ByteBankImportacaoExportacao/3_CriandoArquivo.cs
@@ -0,0 +1,56 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaConmoSting = "456,7895,4785.40, Gustavo Santos";
+
+                var encode = Encoding.UTF8;
+                var bytes = encode.GetBytes(contaConmoSting);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        static void CriarArquivoComWriter()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                escritor.Write("456,65465,456.0,Pedro");
+            }
+        }
+
+        static void TestaEscrita() 
+        {
+            var caminhoArquivo = "teste.txt";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                for (int i = 0; i < 1000000000; i++)
+                {
+                    escritor.Flush(); // Despeja o buffer para o Stream
+
+                    Console.WriteLine($"Linha {i} foi escrita no arquivo. Tecle enter p adicionar mais uma!");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ C# - .NET/C# Parte 9 - Entrada e saída (IO) com streams/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,32 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao 
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //CriarArquivo();
+            //CriarArquivoComWriter();
+            //EscritaBinaria();
+            //LeituraBinaria();
+
+            File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+
+            Console.WriteLine("Arquivo escrevendoComAClasseFile criado!");
+
+            var bytesArquivo = File.ReadAllBytes("contas.txt");
+            Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+            UsarStreamDeEntrada();
+            Console.WriteLine("Aplicação finalizada");
+            Console.ReadLine();
+        }
+    }
+} 
+ 
