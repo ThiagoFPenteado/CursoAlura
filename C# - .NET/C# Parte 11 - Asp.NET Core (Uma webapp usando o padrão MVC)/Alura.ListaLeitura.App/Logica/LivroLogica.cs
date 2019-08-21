979b9ab55452bd23370b0cd0e42742e6493cd79f
Thiago Penteado <thiago.penteado@to-brasil.com>
2019-08-21 16:09:09 -0300

    aula - Asp.NET Core Uma webapp usando o padrão MVC

---
+++ C# - .NET/C# Parte 11 - Asp.NET Core (Uma webapp usando o padrão MVC)/Alura.ListaLeitura.App/Logica/LivroLogica.cs
@@ -0,0 +1,63 @@
+﻿using Alura.ListaLeitura.App.HTML;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public class LivrosController : Controller
+    {
+        public IActionResult ParaLer(HttpContext contexto)
+        {
+            //var html = HtmlUtils.CarregaArquivoHTML("para-ler");
+            var _repo = new LivroRepositorioCSV();
+
+
+            //foreach (var livro in _repo.ParaLer.Livros)
+            //{
+            //    html = html.Replace("#NOVO-ITEM#", $"<li>{livro.Titulo} - {livro.Autor}</li>#NOVO-ITEM#");
+            //}
+            //html = html.Replace("#NOVO-ITEM#", "");
+            //return contexto.Response.WriteAsync(html);
+            ViewBag.Livros = _repo.ParaLer.Livros;
+            //var html = new ViewResult { ViewName = "lista" };
+            return View("lista");
+        }
+
+        public IActionResult Lendo(HttpContext contexto)
+        {
+            //var _repo = new LivroRepositorioCSV();
+            //return contexto.Response.WriteAsync(_repo.Lendo.ToString());
+            var _repo = new LivroRepositorioCSV();
+            ViewBag.Livros = _repo.Lendo.Livros;
+            return View("lista");
+        }
+        public IActionResult Lidos(HttpContext contexto)
+        {
+            //var _repo = new LivroRepositorioCSV();
+            //return contexto.Response.WriteAsync(_repo.Lidos.ToString());
+            var _repo = new LivroRepositorioCSV();
+            ViewBag.Livros = _repo.Lidos.Livros;
+            return View("lista");
+        }
+        public string Detalhes(int id)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var livro = _repo.Todos.First(l => l.Id == id);
+            return livro.Detalhes();
+        }
+
+        public string Teste()
+        {
+            return "nova funcionabilidade implementada";
+        }
+    }
+}
---
+++ C# - .NET/C# Parte 11 - Asp.NET Core (Uma webapp usando o padrão MVC)/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddRouting();
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            //app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
+
+        }
+    }
+}
+
+//codigo comentado
+//public Task Roteamento (HttpContext context)
+        //{
+        //    var _repo = new LivroRepositorioCSV();
+        //    var caminhosAtendidos = new Dictionary<string, RequestDelegate>
+        //    {
+        //        { "/Livros/ParaLer", LivrosParaLer },
+        //        { "/Livros/Lendo", LivrosLendo },
+        //        { "/Livros/Lidos", LivrosLidos }
+        //    };
+
+        //    if (caminhosAtendidos.ContainsKey(context.Request.Path))
+        //    {
+        //        var metodo = caminhosAtendidos[context.Request.Path];
+        //        return metodo.Invoke(context);
+        //    }
+
+        //    context.Response.StatusCode = 404;
+        //    return context.Response.WriteAsync("Caminho inexistente.");
+        //}        
+
+        //public void comentario()
+        //{
+        //    //Metodo antigo
+
+        //    //var routeBuilder = new RouteBuilder(app);
+        //    //routeBuilder.MapRoute("Livros/ParaLer", LivroLogica.ParaLer);
+        //    //routeBuilder.MapRoute("Livros/Lendo", LivroLogica.Lendo);
+        //    //routeBuilder.MapRoute("Livros/Lidos", LivroLogica.Lidos);
+        //    //routeBuilder.MapRoute("Cadastro/NovoLivro/{nome}/{autor}", CadastroLogica.NovoLivro);
+        //    //routeBuilder.MapRoute("Livros/Detalhes/{id:int}", LivroLogica.Detalhes);
+        //    //routeBuilder.MapRoute("Cadastro/ExibeFormulario", CadastroLogica.ExibeFormulario);
+        //    //routeBuilder.MapRoute("Cadastro/Incluir", CadastroLogica.Incluir);
+
+        //    //var rotas = routeBuilder.Build();
+
+        //    //app.UseRouter(rotas);
+
+        //    //app.Run(Roteamento);
+        //}
