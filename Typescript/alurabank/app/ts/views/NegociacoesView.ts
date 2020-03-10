7578a2d11a6fec699be9cf7b62c5bad3b2d956b0
Thiago Penteado <thiago.penteado@to-brasil.com>
2020-03-10 12:21:05 -0300

    NameSpace e módulos externos

--- Typescript/alurabank/app/ts/views/NegociacoesView.ts
+++ Typescript/alurabank/app/ts/views/NegociacoesView.ts
@@ -1,41 +1,37 @@
-namespace Views{
+import { View } from './View';
+import { Negociacoes } from '../models/index';
 
-    // Para evitar repitir Views.View
-    // import view = Views.View
-    // export class NegociacoesView extends view<Negociacoes> {
+export class NegociacoesView extends View<Negociacoes> {
 
-    export class NegociacoesView extends Views.View<Negociacoes> {
+    template(model: Negociacoes): string {
 
-        template(model: Negociacoes): string {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
 
-            return `
-            <table class="table table-hover table-bordered">
-                <thead>
+            <tbody>
+                ${model.paraArray().map(negociacao => 
+                    `
                     <tr>
-                        <th>DATA</th>
-                        <th>QUANTIDADE</th>
-                        <th>VALOR</th>
-                        <th>VOLUME</th>
-                    </tr>
-                </thead>
+                        <td>${negociacao.data.getDate()}/${negociacao.data.getMonth()+1}/${negociacao.data.getFullYear()}</td>
+                        <td>${negociacao.quantidade}</td>
+                        <td>${negociacao.valor}</td>
+                        <td>${negociacao.volume}</td>
+                    </tr>                        
+                    `
+                ).join('')}            
+            </tbody>
 
-                <tbody>
-                    ${model.paraArray().map(negociacao => 
-                        `
-                        <tr>
-                            <td>${negociacao.data.getDate()}/${negociacao.data.getMonth()+1}/${negociacao.data.getFullYear()}</td>
-                            <td>${negociacao.quantidade}</td>
-                            <td>${negociacao.valor}</td>
-                            <td>${negociacao.volume}</td>
-                        </tr>                        
-                        `
-                    ).join('')}            
-                </tbody>
-
-                <tfoot>
-                </tfoot>
-            </table>               
-            `
-        }
+            <tfoot>
+            </tfoot>
+        </table>               
+        `
     }
 }
