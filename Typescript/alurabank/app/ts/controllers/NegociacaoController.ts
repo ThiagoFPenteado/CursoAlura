7818ad8137c4b4352304cf38dfef115bec55f260
Thiago Penteado <thiago.penteado@to-brasil.com>
2020-03-10 15:09:38 -0300

    Lapidando código

--- Typescript/alurabank/app/ts/controllers/NegociacaoController.ts
+++ Typescript/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -20,6 +20,14 @@ export class NegociacaoController {
     adiciona(event: Event) {
         event.preventDefault(); //evitar carregar a pagina
 
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if(!this._ehDiaUtil(data)) {
+
+            this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+            return 
+        }
+
         const negociacao = new Negociacao(
             new Date(this._inputData.val().replace(/-/g, ',')),
             parseInt(this._inputQuantidade.val()),
@@ -37,4 +45,19 @@ export class NegociacaoController {
         this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update('Negociação adicionada com sucesso');
     }
+
+    private _ehDiaUtil(data: Date) {
+
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+}
+
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta, 
+    Quinta, 
+    Sexta, 
+    Sabado
 }
