bdf8e61a0c9317f5b25c5d745142fb0f2409e77a
Thiago Penteado <thiago.penteado@to-brasil.com>
2020-03-10 16:35:06 -0300

    utilizando API externa e interface

--- Typescript/alurabank/app/ts/controllers/NegociacaoController.ts
+++ Typescript/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,24 +1,31 @@
 import { NegociacoesView, MensagemView } from '../views/index';
-import { Negociacoes, Negociacao } from '../models/index';
+import { Negociacoes, Negociacao, NegociacaoParcial } from '../models/index';
+import { domInject, throttle } from '../helpers/decorators/index';
+import { NegociacaoService } from '../services/index';
 
 export class NegociacaoController {
 
+    @domInject('#data')
     private _inputData: JQuery;
+    @domInject('#quantidade')
     private _inputQuantidade: JQuery;
+    @domInject('#valor')
     private _inputValor: JQuery;
+
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView');
-    
+    private _service = new NegociacaoService();
 
     constructor() {
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
+        // this._inputData = $('#data');
+        // this._inputQuantidade = $('#quantidade');
+        // this._inputValor = $('#valor');
         this._negociacoesView.update(this._negociacoes);
     }
-    adiciona(event: Event) {
-        event.preventDefault(); //evitar carregar a pagina
+    @throttle()
+    adiciona() {
+        // event.preventDefault(); //evitar carregar a pagina
 
         let data = new Date(this._inputData.val().replace(/-/g, ','));
 
@@ -49,6 +56,20 @@ export class NegociacaoController {
     private _ehDiaUtil(data: Date) {
 
         return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+
+    @throttle()
+    importaDados() {
+        this._service
+            .obterNegociacoes(res => {
+                if(res.ok) return res;
+                throw new Error(res.statusText);
+            })
+            .then(negociacoes => {
+                negociacoes.forEach(negociacao => 
+                    this._negociacoes.adiciona(negociacao));
+                this._negociacoesView.update(this._negociacoes);
+            });
     }
 }
 
